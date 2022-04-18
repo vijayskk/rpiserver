1d13d6d68192f1070a6030a4147abeddc6efb8d1
vijayskk <vijayskk007@gmail.com>
2022-04-18 14:35:08 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const {readFileSync,writeFileSync} = require('fs')
+
+const express = require('express')
+
+const app = express()
+
+app.get('/',(req,res)=>{
+    const count = readFileSync('./count.txt')
+    console.log('count : ',count)
+    const newCount = parseInt(count) + 1
+    writeFileSync('./count.txt',`${newCount}`)
+    
+    res.send(`
+        <DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="utf-8" />
+            <meta name="viewport " content="width=device - width, initial- scale=1" />
+            <title>RP1 Hosted website</title>
+        </head>
+        <body>
+        <h1>Welcome to my Website !</h1>
+        <p>This page has been viewed ${newCount} times ! </p>
+        </body>
+        </html>
+    `)
+})
+
+app.listen(5000, ()=>{
+    console.log('http://localhost:5000/')
+})
